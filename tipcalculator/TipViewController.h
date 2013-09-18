fd8d55aead9928fc30ae6e2bf23aa3dcb7a8c053
Jeremy Reyes <djreyes@gmail.com>
2013-09-17 23:48:00 -0700

    Complete Tip Calculator Walkthrough

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Jeremy Reyes on 9/17/13.
+//  Copyright (c) 2013 Jeremy Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
