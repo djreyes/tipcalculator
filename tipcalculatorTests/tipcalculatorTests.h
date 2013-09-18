deccf9b28b30bd6d2767c72da6fbfe52b52d46c2
Jeremy Reyes <djreyes@gmail.com>
2013-09-17 22:54:27 -0700

    Initial Commit

---
+++ tipcalculatorTests/tipcalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  tipcalculatorTests.h
+//  tipcalculatorTests
+//
+//  Created by Jeremy Reyes on 9/17/13.
+//  Copyright (c) 2013 Jeremy Reyes. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface tipcalculatorTests : SenTestCase
+
+@end
